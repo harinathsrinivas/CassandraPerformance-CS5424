505e895496b4e03ac62f63cc35a513253557e204
harinathsrinivas <harinathsrinivas94@gmail.com>
2019-10-10 15:56:18 +0800

    Commit changes for large tables based on partition keys

--- src/Performance.java
+++ src/Performance.java
@@ -1,3 +1,4 @@
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -38,7 +39,6 @@ public class Performance {
         session = cluster.connect(keyspace);
     }
     public static void executePerformanceQueries(String keyspaceName) {
-        List<String> queryList = new ArrayList<>();
         int retry = 10;
         while(true && (retry--)>0) {
             try {
@@ -53,41 +53,60 @@ public class Performance {
                 rs = session.execute(query2);
                 row = rs.one();
                 System.out.println("Query 2");
-                System.out.println("sum(W YTD) \t sum(D_NEXT_O_ID)");
-                System.out.println(row.getDecimal(0) + "\t" + row.getInt(1));
-                queryList.add(query2);
+                System.out.println("sum(W YTD) \tsum(D_NEXT_O_ID)");
+                System.out.println(row.getDecimal(0) + "\t" +  row.getInt(1));
 
                 String query3 = "select sum(C_BALANCE), sum(C_YTD_PAYMENT), sum(C_PAYMENT_CNT), sum(C_DELIVERY_CNT) from Customers;";
                 rs = session.execute(query3);
                 row = rs.one();
                 System.out.println("Query 3");
-                System.out.println("sum(C_BALANCE) \t sum(C_YTD_PAYMENT) \t sum(C_PAYMENT_CNT) \t sum(C_DELIVERY_CNT)");
-                System.out.println(row.getDecimal(0) + "\t" + row.getFloat(1) + "\t" + row.getInt(2) + "\t" + row.getInt(3));
-                queryList.add(query3);
+                System.out.println("sum(C_BALANCE) \tsum(C_YTD_PAYMENT) \tsum(C_PAYMENT_CNT) \tsum(C_DELIVERY_CNT)");
+                System.out.println(row.getDecimal(0) + "\t" +  row.getFloat(1) + "\t\t" + row.getInt(2) + "\t\t\t" + row.getInt(3));
 
                 String query4 = "select max(O_ID), sum(O_OL_CNT) from Orders;";
                 rs = session.execute(query4);
                 row = rs.one();
                 System.out.println("Query 4");
-                System.out.println("max(O_ID) \t sum(O_OL_CNT)");
-                System.out.println(row.getInt(0) + "\t" + row.getDecimal(1));
-                queryList.add(query4);
+                System.out.println("max(O_ID) \tsum(O_OL_CNT)");
+                System.out.println(row.getInt(0) + "\t\t" + row.getDecimal(1));
 
-                String query5 = "select sum(OL_AMOUNT), sum(OL_QUANTITY) from OrderLines;";
-                rs = session.execute(query5);
-                row = rs.one();
+                // Query 5
+                BigDecimal OL_AMOUNT= new BigDecimal(0);
+                BigDecimal OL_QUANTITY= new BigDecimal(0);
+
+                for(int i=1; i<=10; i++)
+                    for(int j=1; j<=10; j++){
+                        String query5 = "select sum(OL_AMOUNT), sum(OL_QUANTITY) from OrderLines where OL_W_ID ="+i+" and OL_D_ID="+j+";";
+                        rs = session.execute(query5);
+                        row = rs.one();
+                        OL_AMOUNT = OL_AMOUNT.add(row.getDecimal(0));
+                        OL_QUANTITY = OL_QUANTITY.add(row.getDecimal(1));
+                        //System.out.println("For w_id"+i+" d_id"+j+" OL_AMT"+OL_AMOUNT+" OL_QTY"+OL_QUANTITY);
+                    }
                 System.out.println("Query 5");
-                System.out.println(" sum(OL_AMOUNT) \t  sum(OL_QUANTITY)");
-                System.out.println(row.getDecimal(0) + "\t" + row.getDecimal(1));
-                queryList.add(query5);
+                System.out.println(" sum(OL_AMOUNT) \tsum(OL_QUANTITY)");
+                System.out.println(OL_AMOUNT + "\t\t" + OL_QUANTITY);
 
-                String query6 = "select sum(S_QUANTITY), sum(S_YTD), sum(S_ORDER_CNT), sum(S_REMOTE_CNT) from Stock;";
-                rs = session.execute(query6);
-                row = rs.one();
+
+                // Query 6
+                BigDecimal S_QUANTITY= new BigDecimal(0);
+                BigDecimal S_YTD= new BigDecimal(0);
+                int S_ORDER_CNT = 0;
+                int S_REMOTE_CNT = 0;
+                for(int i=1; i<=10; i++)
+                    for(int j=1; j<=100000; j++){
+                        String query6 = "select sum(S_QUANTITY), sum(S_YTD), sum(S_ORDER_CNT), sum(S_REMOTE_CNT) from Stock where s_w_id = "+i+" and s_i_id = "+j+";";
+                        rs = session.execute(query6);
+                        row = rs.one();
+                        S_QUANTITY = S_QUANTITY.add(row.getDecimal(0));
+                        S_YTD = S_YTD.add(row.getDecimal(1));
+                        S_ORDER_CNT += row.getInt(2);
+                        S_REMOTE_CNT += row.getInt(3);
+                    }
+
                 System.out.println("Query 6");
-                System.out.println(" sum(S_QUANTITY) \t sum(S_YTD) \t sum(S_ORDER_CNT) \t sum(S_REMOTE_CNT)");
-                System.out.println(row.getDecimal(0) + "\t" + row.getDecimal(1) + "\t" + row.getInt(2) + "\t" + row.getInt(3));
-                queryList.add(query6);
+                System.out.println("sum(S_QUANTITY) \tsum(S_YTD) \tsum(S_ORDER_CNT) \tsum(S_REMOTE_CNT)");
+                System.out.println(S_QUANTITY + "\t" + S_YTD + "\t\t" + S_ORDER_CNT + "\t\t\t" + S_REMOTE_CNT);
                 break;
             } catch (ReadTimeoutException e) {
                 e.printStackTrace();
